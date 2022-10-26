83c6204f7ff8104d2a47e867854eaacb91a3131f
awwaloyetunde <oyetundeawwal@yahoo.com>
2022-10-25 19:12:39 -0500

    Checking

--- shapeCaretaker.java
+++ shapeCaretaker.java
@@ -3,3 +3,4 @@ package Patterns;
 public class shapeCaretaker {
 
 }
+private commandInvoker
--- shapeStateMemento.java
+++ shapeStateMemento.java
@@ -1,6 +1,22 @@
 package Patterns;
 
-public class shapeStateMemento {
-	
-	
+public static class shapeStateMemento {
+    private final String string;
+    private final Editor editor;
+
+
+    public shapeStateMemento(Editor editor) {
+        this.editor = editor;
+        this.backup = editor.backup();
+    }
+
+    private String getSaveText() { //Getter only accessible by outer class or string class
+        return sring;
+    }
+
+    //The momento should have the attributes as the text area
+
+    //public void restore() {
+        //editor.restore(backup);
+    //}
 }
